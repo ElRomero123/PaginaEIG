02cb9aaa2e4fcedfd8b55252d189a900e786c19a
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2019-01-06 14:09:58 -0500

    Cambios

--- EIGApp/EIGApp/js/index.js
+++ EIGApp/EIGApp/js/index.js
@@ -42,9 +42,9 @@ function search(opcion)
                         $('#bannerState').css('background','green');
                         $('#bannerState').css('color','white');
                         $('#bannerState').text(i + ' resultado(s) encontrados!');
-                        $('#listResults').css('display','inline-block');
-                        $('#maps').css('display','inline-block');
-                        $('#maps').append('<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d757.0218273722048!2d-73.9316468375744!3d40.62795646258929!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sco!4v1546800277209" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>');
+                        $('#listResults').css('display','flex');
+                        $('#maps').append('<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d757.0218273722048!2d-73.9316468375744!3d40.62795646258929!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sco!4v1546800277209" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>');
+                        $('#maps').css('display','inline-block');     
                     }
 
                     else
@@ -56,6 +56,7 @@ function search(opcion)
                 }
             }
         );
+
         break;
 
         case 2:
