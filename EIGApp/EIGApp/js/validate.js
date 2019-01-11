97e71f5e67f4f73717e9af85a53d32e9ab0d5f5f
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2019-01-11 13:23:45 -0500

    Cambios ...

---
+++ EIGApp/EIGApp/js/validate.js
@@ -0,0 +1,10 @@
+function option(num)
+{
+    switch(num)
+    {
+        case 1: alert('Opción 1: Validar usuario');
+        break;
+        case 2: alert('Opción 2: Crear usuario nuevo');
+        break;
+    }
+}
