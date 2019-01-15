ac288c19c6937c9e7cbf7d5dc5be8a8f9b170a67
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2019-01-14 22:03:55 -0500

    Cambiar

--- EIGApp/EIGApp/js/panel.js
+++ EIGApp/EIGApp/js/panel.js
@@ -43,7 +43,7 @@ function create(num)
     {
         case 1: //alert('Crear persona funciona');
         loadAvatar(createPerson());
-        //putAvatar();
+        putAvatar();
         break;
         default: alert('Crear otro tipo funciona');
     }
@@ -128,8 +128,7 @@ function createPerson()
 
 function putAvatar()
 {
-    alert("Put Avatar funciona");
-    
+    //alert("Put Avatar funciona");
     $.ajax
     (
         {
