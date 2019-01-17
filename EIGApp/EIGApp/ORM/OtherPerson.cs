f4441d9565dbcacddbf1a4c7e5e43a30226fde99
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2019-01-17 18:24:02 -0500

    Cambios en la App

--- EIGApp/EIGApp/ORM/OtherPerson.cs
+++ EIGApp/EIGApp/ORM/OtherPerson.cs
@@ -30,7 +30,7 @@ namespace EIGApp.ORM
         public string City { get; set; }
         public string Address { get; set; }
         public string Avatar { get; set; }
-        public bool Approved { get; set; }
+        public bool Active { get; set; }
         public long IdUser { get; set; }
     
         public virtual User User { get; set; }
