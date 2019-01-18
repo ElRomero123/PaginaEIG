5330f449ce88d212162fea89ad20907961be4668
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2019-01-18 16:38:01 -0500

    Cambios

---
+++ EIGApp/EIGApp/js/businessProductPanel.js
@@ -0,0 +1,18 @@
+window.onload = initUser;
+
+function initUser()
+{
+    var name     = localStorage.getItem('Name');
+    var username = localStorage.getItem('Username');
+    
+    if(name != null)
+    {
+        $('#infoName').text(name);
+        $('#infoUsername').text(username);
+    }
+
+    else
+    {
+        location.href = 'index.html';
+    }
+}
