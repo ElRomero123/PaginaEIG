9e06e7a088f9d721bd3e688b6b10804035884b91
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2019-01-19 19:05:01 -0500

    Cambios en la App

---
+++ EIGApp/EIGApp/js/personPanel.js
@@ -0,0 +1,193 @@
+window.onload = initUser;
+
+function initUser()
+{
+    var name     = localStorage.getItem('Name');
+    var username = localStorage.getItem('Username');
+    
+    if(name != null)
+    {
+        $('#infoName').text(name);
+        $('#infoUsername').text(username);
+    }
+
+    else
+    {
+        location.href = 'index.html';
+    }
+}
+
+function to(num)
+{
+    switch(num)
+    {
+        case 1: 
+        localStorage.clear();
+        location.href = 'index.html';
+        break;
+        default: 
+        location.href = 'menu.html';
+    }
+}
+
+function createPerson()
+{
+    var persona =
+    {
+        name: $('#campoFullName').val(),
+        profesionDescription: $('#campoProfesionDescription').val(),
+        email: $('#campoEmail').val(),
+        phone: $('#campoPhone').val(),
+        city: $('#campoCity').val(),
+        address: $('#campoAddress').val(),
+        avatar: '',
+        approved: false,
+        idUser: localStorage.getItem('User')
+    };
+
+    if(persona.name != null && persona.profesionDescription != null && persona.email != null && persona.phone != null && persona.city != null && persona.address != null)
+    {
+        $('#createPerson').css('background','yellow');
+        $('#createPerson').css('border','2px solid yellow');
+        $('#createPerson').css('color','black');
+        $('#createPerson').text('Creando perfil de usuario...');
+
+        $.ajax
+        (
+            {
+                url: '../api/person',
+                type: 'POST',
+                data: JSON.stringify(persona),
+                contentType: "application/json;charset=utf-8",
+
+                success:
+                function (data)
+                {
+                    loadAvatar(data);
+                }
+            }
+        );
+    }
+
+    else
+    {
+        $('#createPerson').css('background','red');
+        $('#createPerson').css('border','2px solid red');
+        $('#createPerson').text('Caracteres y digitos insuficientes!');
+    }
+}
+
+function loadAvatar(num)
+{    
+    var config = 
+    {
+        apiKey: "AIzaSyA4F7aYKhXv5zEWabtUYABA-4lJJdAgyW4",
+        authDomain: "eliteintelligencegroup-719d3.firebaseapp.com",
+        databaseURL: "https://eliteintelligencegroup-719d3.firebaseio.com",
+        projectId: "eliteintelligencegroup-719d3",
+        storageBucket: "eliteintelligencegroup-719d3.appspot.com",
+        messagingSenderId: "567347907651"
+    };
+    
+    firebase.initializeApp(config);
+
+    var campoAvatarPerson = document.getElementById('fileBrowser');
+    var storageRef        = firebase.storage().ref();
+    
+    var AvatarPerson = campoAvatarPerson.files[0];
+
+    if(AvatarPerson != null)
+    {
+        var uploadTask = storageRef.child('avatar/' + 'P' + num).put(AvatarPerson);
+
+        uploadTask.on
+        (   
+            'state_changed',
+            function(snapshot)
+            {
+    
+            },
+            function(error)
+            {
+                
+            },
+            function()
+            {
+                uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) 
+                {    
+                    putAvatar(num, downloadURL);
+                });
+            }
+        );
+    }
+
+    else
+    {
+        $('#createPerson').css('background','red');
+        $('#createPerson').css('border','2px solid red');
+        $('#createPerson').text('NO existe foto de usuario!');
+
+        setTimeout(recargar, 2500);
+    }
+}
+
+function putAvatar(num, downloadURL)
+{
+    var parametrosPutAvatar =
+    {
+        idPerson: num,
+        downloadURL: downloadURL
+    };
+
+    $.ajax
+    (
+        {
+            url: '../api/parametros',
+            type: 'POST',
+            data: JSON.stringify(parametrosPutAvatar),
+            contentType: "application/json;charset=utf-8",
+
+            success:
+            function (data)
+            {
+                if(data)
+                {
+                    $('#createPerson').css('background','darkgreen');
+                    $('#createPerson').css('border','2px solid darkgreen');
+                    $('#createPerson').css('color','white');
+                    $('#createPerson').text('Perfil ingresado con éxito!');
+                    
+                    setTimeout(recargar, 2500);
+                }
+            }
+        }
+    );
+}
+
+document.getElementById("fileBrowser").onchange = 
+function(e) 
+{
+    let reader = new FileReader();
+  
+    reader.readAsDataURL(e.target.files[0]);
+    
+    console.log(e.target.files[0]);
+    reader.onload = function()
+    {
+        let preview = document.getElementById('preview'),
+        image = document.createElement('img');
+
+        image.src = reader.result;
+
+        image.id = 1;
+        var imagen = $('#1');
+
+        preview.innerHTML = '';
+        preview.append(image);
+    };
+}
+
+function recargar()
+{
+    location.reload();
+}
