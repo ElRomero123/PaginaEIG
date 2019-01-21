740219cf58ad7de151e110bf77489929e4db4d55
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2019-01-21 17:13:46 -0500

    Cambios

---
+++ EIGApp/EIGApp/obj/Release/Package/PackageTmp/js/menu7.js
@@ -0,0 +1,43 @@
+window.onload = initUser;
+
+function initUser()
+{
+    var name     = localStorage.getItem('Name');
+    var username = localStorage.getItem('Username');
+    
+    if(name != null)
+    {
+        $('#infoName').text(name);
+        $('#infoUsername').text(username);
+    }
+
+    else
+    {
+        location.href = 'index.html';
+    }
+}
+
+function to(num)
+{
+    switch(num)
+    {
+        case 1:
+        localStorage.clear();
+        location.href = 'index.html';
+        break;
+        case 2:
+        location.href = 'menu.html';
+        break;
+        case 3:
+        location.href = 'personPanelC.html';
+        break;
+        case 4:
+        location.href = 'otherPersonPanelC.html';
+        break;
+        case 5:
+        location.href = 'businessPanel.html';
+        break;
+        default:
+        location.href = 'productPanel.html';
+    }
+}
