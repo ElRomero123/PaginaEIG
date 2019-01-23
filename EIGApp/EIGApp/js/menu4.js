b48abd59811bedc352d2d16b8515913c83e1a9f0
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2019-01-23 09:20:44 -0500

    Cambios

--- EIGApp/EIGApp/js/menu4.js
+++ EIGApp/EIGApp/js/menu4.js
@@ -46,7 +46,16 @@ function search()
                         
                         for(var i = 0; i < data.length; i++)
                         {
-                            cadena += "<div class='result'> <div class='avatar' id='" + i + "'></div> <div class='text'> <p class='pf1'>" + data[i].Name + "</p> <p class='pf2'>" + data[i].Type + "</p> <p class='pf2'>" + data[i].TypeDescription + "</p> <p class='pf3'>" + data[i].AttendantName + "</p> <p class='pf3'>" + data[i].AttendantPhone + "</p>  <p class='pf3'>" + data[i].AttendantEmail + "</p> <p class='pf4'>" + data[i].City + "</p> <p class='pf4'>" + data[i].Address + "</p> <p class='pf4'>" + data[i].Date + "</p> <p class='c'>Pertenece a CIPRIN</p> </div> </div>";  
+                            if(data[i].Ciprin == 1)
+                            {
+                                cadena += "<div class='result'> <div class='avatar' id='" + i + "'></div> <div class='text'> <p class='pf1'>" + data[i].Name + "</p> <p class='pf2'>" + data[i].Type + "</p> <p class='pf2'>" + data[i].TypeDescription + "</p> <p class='pf3'>" + data[i].AttendantName + "</p> <p class='pf3'>" + data[i].AttendantPhone + "</p>  <p class='pf3'>" + data[i].AttendantEmail + "</p> <p class='pf4'>" + data[i].City + "</p> <p class='pf4'>" + data[i].Address + "</p> <p class='pf4'>" + data[i].Date + "</p> <p class='c'>Pertenece a CIPRIN</p> </div> </div>";
+                            }
+                            else
+                            {
+                                cadena += "<div class='result'> <div class='avatar' id='" + i + "'></div> <div class='text'> <p class='pf1'>" + data[i].Name + "</p> <p class='pf2'>" + data[i].Specialism + "</p> <p class='pf2'>" + data[i].SpecialismDescription + "</p> <p class='pf3'>" + data[i].WebPage + "</p> <p class='pf4'>" + data[i].Phone + "</p> <p class='pf4'>" + data[i].City + "</p> <p class='pf4'>" + data[i].Address + "</p> </div> </div>";
+                            }
+
+                              
                         } 
 
                         $('#listResults').append(cadena);
