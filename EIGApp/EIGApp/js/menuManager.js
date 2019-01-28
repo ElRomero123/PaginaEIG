81fe5e8b6163b67b2ce4a4c637d58ef7b9a4aa0e
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2019-01-28 18:45:57 -0500

    Cambios

--- EIGApp/EIGApp/js/menuManager.js
+++ EIGApp/EIGApp/js/menuManager.js
@@ -17,39 +17,12 @@ function initUser()
     }
 }
 
-/*
+
 function to(num)
 {
     switch(num)
     {
         case 1:
-        location.href = 'menu.html';
-        break;
-        case 2:
-        location.href = 'menu2.html';
-        break;
-        case 3:
-        location.href = 'menu3.html';
-        break;
-        case 4:
-        location.href = 'productos.html';
-        break;
-        case 5:
-        location.href = 'menu5.html';
-        break;
-        case 6:
-        location.href = 'menu6.html';
-        break;
-        case 7:
-        location.href = 'menu7.html';
-        break;
-        case 8:
-        location.href = 'menu8.html';
-        break;
-        case 9:
-        location.href = 'menu9.html';
-        break;
-        case 10:
         localStorage.clear();
         location.href = 'index.html';
         break;
@@ -57,4 +30,3 @@ function to(num)
         location.href = 'personPanel.html';
     }
 }
-*/
