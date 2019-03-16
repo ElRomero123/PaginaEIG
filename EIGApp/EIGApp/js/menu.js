076cc1d751f67713f7b1b236cfced371455c54ad
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2019-03-16 17:41:50 -0500

    Cambios

--- EIGApp/EIGApp/js/menu.js
+++ EIGApp/EIGApp/js/menu.js
@@ -56,23 +56,26 @@ function search()
                 if(data.length > 0)
                 {
                     var chain = new StringBuilder();
-                    var card;
+                    var card, card2;
                     for(var i = 0; i < data.length; i++)
                     {
                         if(data[i].Ciprin)
                         {
                             card = "<div class='result'> <div class='avatar' id='" + data[i].Id + "'></div> <div class='text'> <p class='pf1'>" + data[i].Name + "</p> <p class='pf2'>" + data[i].ProfesionDescription + "</p> <p class='pf2'>" + data[i].Email + "</p> <p class='pf2'>" + data[i].Phone + "</p> <p class='pf2'>Creado el " + data[i].CreationDate + ' por ' + data[i].Username +  "</p> <p class='pf4'>" + data[i].CreationHourZone +  "</p> <button id='" + data[i].Id + "' class='moreResult' onclick='showMedia(this)'>Multimedia</button> <p style='background:green; color:white; padding: 4px;'>Afiliado a CIPRIN</p> <p class='pf3'>" + data[i].Views +  " visitas</p> </div> </div>";
+                            card2 = "<div class='text'> <p class='pf1' style='color: black'>" + data[i].Name + "</p> <p class='pf2'>" + data[i].ProfesionDescription + "</p> <p class='pf2'>" + data[i].Email + "</p> <p class='pf2'>" + data[i].Phone + "</p> <p class='pf2'>Creado el " + data[i].CreationDate + ' por ' + data[i].Username +  "</p> <p class='pf4'>" + data[i].CreationHourZone +  "</p> <button id='" + data[i].Id + "' class='moreResult' onclick='showMedia(this)'>Multimedia</button> <p style='background:green; color:white; padding: 4px;'>Afiliado a CIPRIN</p> <p class='pf3'>" + data[i].Views +  " visitas</p> </div>";
                             chain.append(card);
-                            putMarker(latLng, card);
+                            putMarker({lat:data[i].Latitude,lng:data[i].Longitude}, card2);
                         }
 
                         else
                         {
                             card = "<div class='result'> <div class='avatar' id='" + data[i].Id + "'></div> <div class='text'> <p class='pf1'>" + data[i].Name + "</p> <p class='pf2'>" + data[i].ProfesionDescription + "</p> <p class='pf2'>" + data[i].Email + "</p> <p class='pf2'>" + data[i].Phone + "</p> <p class='pf2'>Creado el " + data[i].CreationDate + ' por ' + data[i].Username +  "</p> <p class='pf4'>" + data[i].CreationHourZone +  "</p> <button id='" + data[i].Id + "' class='moreResult' onclick='showMedia(this)'>Multimedia</button> <p class='pf3'>" + data[i].Views +  " visitas</p> </div> </div>"
+                            card2 = "<div class='text'> <p class='pf1'>" + data[i].Name + "</p> <p class='pf2'>" + data[i].ProfesionDescription + "</p> <p class='pf2'>" + data[i].Email + "</p> <p class='pf2'>" + data[i].Phone + "</p> <p class='pf2'>Creado el " + data[i].CreationDate + ' por ' + data[i].Username +  "</p> <p class='pf4'>" + data[i].CreationHourZone +  "</p> <button id='" + data[i].Id + "' class='moreResult' onclick='showMedia(this)'>Multimedia</button> <p class='pf3'>" + data[i].Views +  " visitas</p> </div>";
                             chain.append(card);
-                            putMarker(latLng, card);
+                            putMarker({lat:data[i].Latitude,lng:data[i].Longitude}, card2);
                         }
                     }
+
                     $('#listResults').css('display','flex');
                     $('#listResults').append(chain.toString());
                     chain.clear();
@@ -197,37 +200,6 @@ function initMap()
     {
        crd = pos.coords;
        map = new google.maps.Map(document.getElementById('maps'), {zoom: 15, center: {lat: crd.latitude, lng: crd.longitude}});
-       new google.maps.Marker({
-        position: {lat: crd.latitude, lng: crd.longitude},
-        map: map,
-        title: 'Hello World!'
-        });
-
-        var ca = '<div id="content">'+
-    '<div id="siteNotice">'+
-    '</div>'+
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-    '<div id="bodyContent">'+
-    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-    'sandstone rock formation in the southern part of the '+
-    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-    'south west of the nearest large town, Alice Springs; 450&#160;km '+
-    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-    'Aboriginal people of the area. It has many springs, waterholes, '+
-    'rock caves and ancient paintings. Uluru is listed as a World '+
-    'Heritage Site.</p>'+
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-    'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-    '(last visited June 22, 2009).</p>'+
-    '</div>'+
-    '</div>';
-
-        for(var i = 0; i < 4; i++)
-        {
-            putMarker({lat: crd.latitude + i/1000, lng: crd.longitude + i/1000}, ca);
-        }
     }
     function error(){}
     navigator.geolocation.getCurrentPosition(success, error, options);
@@ -236,6 +208,6 @@ function initMap()
 function putMarker(latLng, card)
 {
     var infowindow = new google.maps.InfoWindow({content: card});
-    var marker = new google.maps.Marker({position: latLng, map: map, title: '<font color="green">This is some text!</font> another text'});
+    var marker = new google.maps.Marker({position: latLng, map: map});
     marker.addListener('click', function() {infowindow.open(map, marker);});
 }
